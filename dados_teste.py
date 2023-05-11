0b35591136992ac984f626c69c06584174663640
schinoff <129887252+schin-off@users.noreply.github.com>
2023-05-11 20:26:32 -0300

    Update dados_teste.py

--- dados_teste.py
+++ dados_teste.py
@@ -1,16 +1,18 @@
 import pandas as pd
+import matplotlib
 
-rd = pd.read_csv('dados - Página1.csv')
-rd = rd.drop(columns=['Maxpulse']) #remove the column 'Maxpulse'
-rd = rd.dropna() #removed all NaN
-rd['Date'] = rd.loc[26, 'Date'] = '2020/12/26' #change the row 26 to the right format
+rd = pd.read_csv('dados - Página1.csv')# leio o arquivo
+rd = rd.drop(columns=['Maxpulse'])#retiro a coluna 'Maxpulse'
+rd = rd.drop(26)#retiro a linha 26
+rd = rd.dropna()#retiro todos valores NaN
+rd['Date'] = rd['Date'].str.replace('2020/12/', '') #retiro o mês e o ano por serem os mesmo
 
-# a loop to change all the wrong values in 'Duration'
-for x in rd.index:
+for x in rd.index: #um looping para que todos valores de 'Duration sejam os mesmos
   if rd.loc[x, "Duration"] > 60:
     rd.loc[x, "Duration"] = 60
 for x in rd.index:
   if rd.loc[x, "Duration"] < 60:
     rd.loc[x, "Duration"] = 60
 
-print(rd.to_string())
+print(rd)
+print(rd.plot( 'Date' , 'Calories' ))#crio um grafico
